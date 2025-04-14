a3b55b9719841be5f9e8af3229303e44a6a61dbd
Manish Jha <jhas67761@gmail.com>
2025-04-14 21:41:47 +0530

    Create SubarraySumEqual_toK.java
    Subarray Quesation

---
+++ SubarraySumEqual_toK.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        int n =  nums.length ;
+        int sum = 0;  
+
+        Map<Integer , Integer> mp = new HashMap<>() ;
+        mp.put(0,1) ;  
+        int count  = 0 ;
+
+        for(int i=0; i<n; i++){
+           sum  += nums[i] ;
+
+           if(mp.containsKey(sum-k))
+           {
+            count += mp.get(sum - k) ;
+           } 
+
+           mp.put(sum , mp.getOrDefault(sum,0)+1) ;
+        }
+       return count ;
+    }
+}
+
+
